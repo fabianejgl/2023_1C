8ad543e2bbbd995dc6617f8e55aeabe98b4feaf4
Fabs <fabiangl2011@gmail.com>
2023-04-30 22:19:29 -0300

    detalle

---
+++ SO/Guias/primer_p/pipes_parcial.c
@@ -0,0 +1,51 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+
+
+int nietoTermino = 0;
+int N;
+
+void handler(){
+    nietoTermino = 1;
+}
+
+void ejecutarHijo( int i, int pipes[][2]) {
+    for(int j = 0; j < (N*2); j++){
+        if(j==i || j == N+i){
+            continue;
+        } else {
+            close(pipes[i][0]);
+            close(pipes[i][1]);
+        }
+    }
+    signal(SIGTERM, handler);
+    int numero;
+    read(pipes[i][0],&numero, sizeof(numero));
+    int pipeNieto[2];
+    pipe(pipeNieto);
+    pid_t nieto = fork();
+    if (nieto == 0){
+        ejecutarNieto(i, pipes, pipeNieto, getpid(), numero);
+    } else {
+        while(!nietoTermino){
+
+        }
+        write(pipes[N+i][1], &nietoTermino, sizeof(nietoTermino));
+        int resultado;
+        read(pipeNieto[0], &resultado, sizeof(resultado));
+        write(pipes[N+i][1], &numero, sizeof(numero));
+        write(pipes[N+i][1], &resultado, sizeof(resultado));
+    }
+    exit(0);
+}
+
+
+
+void ejecutarNieto(int i, int pipes[][2], int pipeNieto[2], pid_t padre, int numero) {
+    int resultado = calcular(numero);
+    kill(padre, SIGTERM);
+    write(pipeNieto[1], &resultado, sizeof(resultado));
+    exit(0);
+}
